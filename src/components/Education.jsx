f4a8a4ddfbef0697f4fc509effc372fdd83ac6f0
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-22 10:27:28 +0200

    fix rotating bulletpoints on about page

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -14,7 +14,7 @@ const Education = () => {
       <h2 className='mb-8 text-center text-4xl font-bold sm:mb-16 sm:text-6xl md:text-8xl'>
         Education
       </h2>
-      <div ref={ref} className='relative mx-auto w-full lg:w-[90%] xl:w-[84%]'>
+      <div className='relative mx-auto w-full lg:w-[90%] xl:w-[84%]'>
         <motion.div
           style={{ scaleY: scrollYProgress }}
           // className='absolute left-9 top-0 h-full w-[2px] origin-top bg-dark dark:bg-light'
