f909288cd0a223e01757a27e3d1a1274c038cdc1
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-23 11:23:03 +0200

    fix imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,6 @@ import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import RootLayout from '../components/layouts/RootLayout'
 import { Analytics } from '@vercel/analytics/react'
-;<Head>
-  <meta name='viewport' content='width=device-width,initial-scale=1' />
-  <link rel='icon' href='/favicon.ico' />
-</Head>
 const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-mont' })
 
 export default function App({ Component, pageProps }) {
